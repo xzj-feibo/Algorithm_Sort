5f8db94c2fca8ec50363b0404ff22430fe86edd6
xzj-feibo <126376839+xzj-feibo@users.noreply.github.com>
2023-08-06 17:00:54 +0800

    Update BubbleSort.java

--- src/交换排序/BubbleSort.java
+++ src/交换排序/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
     public static int[] sort(int[] a){
         for (int i = 0; i < a.length; i++) {
             for (int j = 0;j<a.length - i - 1;j++){
-                if (a[j+1] > a[j]){
+                if (a[j] > a[j+1]){
                     int temp=a[j+1];
                     a[j+1]=a[j];
                     a[j]=temp;
